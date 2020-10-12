113c93a09387ff900d33ebb238434a03f6c00548
Raúl Martín <55482385+rmartinsanta@users.noreply.github.com>
2020-10-12 20:27:36 +0200

    Update docker-compose

--- common/install-docker.sh
+++ common/install-docker.sh
@@ -85,7 +85,7 @@ error "Installing Docker, check that your Linux version is supported"
 
 # Download and install Docker Compose
 echo "--> Downloading docker compose..."
-curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 error "Downloading Docker Compose"
 chmod +x /usr/bin/docker-compose
 error "Installing Docker Compose"
