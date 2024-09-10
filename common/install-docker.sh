7cd6526ba4314d59af4663657e79e86da0691565
Raúl Martín <55482385+rmartinsanta@users.noreply.github.com>
2024-09-10 15:25:38 +0200

    Update install-docker.sh

--- common/install-docker.sh
+++ common/install-docker.sh
@@ -62,7 +62,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 error "Add docker signing key"
 
 echo "--> Adding Docker repository..."
-sudo add-apt-repository \
+add-apt-repository -y \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
