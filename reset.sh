8554261e8aeb0a9ac2f9fb94c38b3e88c6d5ad5e
SergioP3rez <sergioperezp1995@gmail.com>
2026-09-09 19:24:05 +0200

    Include instruction for executing install.sh before resetting all data
    Add warning message and delay before data destruction to inform to the user

--- reset.sh
+++ reset.sh
@@ -8,6 +8,8 @@ if (( $# != 1 )); then
 fi
 
 if [ $1 = "--yes-i-really-want-to-destroy-all-data" ]; then
+  echo "If you want to reinstall DOMJudge after reset, please run ./install.sh after reset.  I will wait 10 seconds to make sure you are reading this!"
+  sleep 10
   ./stop.sh
   sudo rm -rf domjudge
   docker volume rm domjudge_sqldata
